1cc801927b6e78e1fa00ccd5986b950270ba930e
Ben Kielty <bwk103@gmail.com>
2017-01-09 15:46:24 +0000

    Adding unit test for default balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,8 +1,12 @@
-
+require 'oystercard'
 
 describe Oystercard do
   it "checks the oystercard has a balance" do
-    expect(Oystercard).to respond to (:balance)
-  end 
+    expect(subject).to respond_to(:balance)
+  end
+
+  it "checks that the Oystercard has a default balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 
 end
