937f6788b0fdac9ccf0703adff366d9ef5cff4e5
frankie <franklin.shaw@gmail.com>
2017-01-13 11:53:28 +0000

    added back station spec

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,9 +1,26 @@
 require 'station'
 
 describe Station do
-  subject(:station) {Station.new("Waterloo", 1)}
 
-  it "should assign entry_station as an instance variable" do
-    expect(station).to have_attributes(:name => "Waterloo", :zone => 1)
+  subject(:bank) { described_class.new "bank" }
+  subject(:leyton) { described_class.new "leyton" }
+
+  describe 'class initiation' do
+    it 'creates an instance of station' do
+      expect(bank).to be_a(Station)
+    end
+    it 'knows its name' do
+      expect(bank.name).to eq("bank")
+    end
   end
+
+  describe 'zone variable' do
+    it 'passes zone to a station when station is created' do
+      expect(bank.zone).to eq(1)
+    end
+    it 'finds different zones for different stations' do
+      expect(leyton.zone).to eq(3)
+    end
+  end
+
 end
