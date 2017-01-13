83c29b0bed1aee02f443af48e50b51b785711f3d
frankie <franklin.shaw@gmail.com>
2017-01-13 11:37:33 +0000

    added back in station read from csv

--- lib/station.rb
+++ lib/station.rb
@@ -1,11 +1,35 @@
+require 'csv'
+
 class Station
 
-attr_reader :name, :zone
+  attr_reader :name
+  attr_reader :zone
 
-def initialize(name, zone)
-  @name = name
-  @zone = zone
-end
+  def initialize(name)
+    @name = name
+    load_stations
+    @zone = define_zone
+  end
 
+  def load_stations(filename = 'stations.csv')
+    raise "No file to load stations from!" if !File.exists?(filename)
+    @catalog = Hash.new
+    load_from_file(filename)
+  end
+
+  private
+
+  def define_zone
+    raise "Station doesn't exist on the map yet." if !@catalog.has_key?(@name)
+    @catalog[@name]
+  end
+
+  def load_from_file(filename)
+    CSV.foreach(filename) do |row|
+      name = row[0]
+      zone = row[1]
+      @catalog.store(name,zone.to_i)
+    end
+  end
 
 end
